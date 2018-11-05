a7f8659ccb2d20e62768acea36bcd96922565a00
Irfan Shaikh <skimirfan@outlook.com>
2018-11-05 13:20:52 +0530

    k

--- Project2/irfan2backend/src/main/java/com/niit/DAO/ProfilePictureDao.java
+++
@@ -1,9 +0,0 @@
-package com.niit.DAO;
-
-import com.niit.Model.ProfilePicture;
-
-public interface ProfilePictureDao {
-	void uploadProfilePicture(ProfilePicture profilePicture);
-	ProfilePicture  getProfilePicture(String email);
-
-}
